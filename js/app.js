0951762735233457957a1c6230d4b517411428fe
caitreid <ctlnrd@gmail.com>
2022-12-16 21:14:26 -0500

    change background color of divs based on turn

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
 
+const square = document.querySelectorAll('.square');
+
+let count = 0;
 
 const choice = (event) => {
 
+    count++ 
+    
+    if (count % 2 === 1) {
+
+        event.target.classList.add('clicked-x')
+
+    }
+
+    if (count % 2 === 0 ) {
+
+        event.target.classList.add('clicked-o')
+    }
     console.log(event.target);
 }
 
+
 document.addEventListener('click', choice)
+
